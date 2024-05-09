a530f238ac1273026fa5b95bd2f8c8d0c2cdf575
ua-fores <a-uno@fores.jp>
2024-05-09 15:13:56 +0900

    Lesson5 オブジェクト指向プログラミング1課題提出

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,72 @@
+package baseball;
+
+public class BaseBallTeam {
+    private String name;
+    private int win;
+    private int lose;
+    private int draw;
+
+    // コンストラクタ
+    // 引数なし
+    public BaseBallTeam() {
+        // 処理なし
+    }
+
+    // 引数あり
+    public BaseBallTeam(String name, int win, int lose, int draw) {
+        this.name = name;
+        this.win = win;
+        this.lose = lose;
+        this.draw = draw;
+    }
+
+    // getter/setter
+//    public String getName() {
+//        return name;
+//    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWin() {
+        return win;
+    }
+
+    public void setWin(int win) {
+        this.win = win;
+    }
+
+    public int getLose() {
+        return lose;
+    }
+
+    public void setLose(int lose) {
+        this.lose = lose;
+    }
+
+    public int getDraw() {
+        return draw;
+    }
+
+    public void setDraw(int draw) {
+        this.draw = draw;
+    }
+
+    // 勝率を計算する
+    public double getRate() {
+        double rate;
+        // 勝ち数/(勝ち数+負け数)
+        rate = (double) win / ((double) win + (double) lose);
+        return rate;
+    }
+
+    // 成績を出力する
+    public void report() {
+        double rate;
+        rate = getRate();
+        System.out.println(name + " の2022年の成績は " + win + "勝 " + lose + "敗 " + draw + "分、勝率は " + rate + "です。");
+
+    }
+
+}
---
+++ src/baseball/Review03.java
@@ -0,0 +1,28 @@
+package baseball;
+
+public class Review03 {
+
+    public static void main(String[] args) {
+        // コンストラクタを利用してデータセットする
+        BaseBallTeam team01 = new BaseBallTeam("東京ヤクルトスワローズ", 80, 59, 4);
+        team01.report();
+        BaseBallTeam team02 = new BaseBallTeam("横浜DeNAベイスターズ", 73, 68, 2);
+        team02.report();
+        BaseBallTeam team03 = new BaseBallTeam("阪神タイガース", 68, 71, 4);
+        team03.report();
+        BaseBallTeam team04 = new BaseBallTeam("読売ジャイアンツ", 68, 72, 3);
+        team04.report();
+        BaseBallTeam team05 = new BaseBallTeam("広島東洋カープ", 66, 74, 3);
+        team05.report();
+
+        // setterを利用してデータセットする
+        BaseBallTeam team06 = new BaseBallTeam();
+        team06.setName("中日ドラゴンズ");
+        team06.setWin(66);
+        team06.setLose(75);
+        team06.setDraw(2);
+        team06.report();
+
+    }
+
+}
