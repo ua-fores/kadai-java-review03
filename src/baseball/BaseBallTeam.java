ecf565752927e17e3d8d5bff4bdb75dda42af8fd
ua-fores <a-uno@fores.jp>
2024-05-09 15:18:18 +0900

    一部getterのコメントアウト取り消し

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -21,9 +21,9 @@ public class BaseBallTeam {
     }
 
     // getter/setter
-//    public String getName() {
-//        return name;
-//    }
+    public String getName() {
+        return name;
+    }
 
     public void setName(String name) {
         this.name = name;
